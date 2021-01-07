3300ac538852c7d85edfcbcc256df5d9600fb3e1
Siyanbola Akin <akin.siyanbola@gmail.com>
2021-01-07 18:22:02 +0100

    Add files via upload

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story { //class
+
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  // properties for class
+
+  Story(this.storyTitle, this.choice1, this.choice2); //a constructor
+
+}
---
+++ lib/story_brain.dart
@@ -0,0 +1,97 @@
+import 'story.dart';
+
+class StoryBrain {
+
+  int _storyNumber = 0;
+
+
+  List<Story> _storyData = [
+ Story(
+     'Your car has blown a tire on a windy road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
+     'I\'ll hop in. Thanks for the help!',
+     'Better ask him if he\'s a murderer first.'),
+ Story(
+     'He nods slowly, unphased by the question.',
+     'At least he\'s honest. I\'ll climb in.',
+    'Wait, I know how to change a tire.'),
+ Story(
+     'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
+     'I love Elton John! Hand him the cassette tape.',
+     'It\'s him or me! You take the knife and stab him.'),
+ Story(
+     'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
+     'Restart',
+     ''),
+ Story(
+     'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
+     'Restart',
+     ''),
+ Story(
+     'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
+     'Restart',
+     '')
+];
+
+void nextStory(int userChoice){
+  if (userChoice == 1 && _storyNumber == 0){
+    _storyNumber = 2;
+  }
+  else if (userChoice == 2 && _storyNumber == 0){
+    _storyNumber = 1;
+  }
+  else if (userChoice == 1 && _storyNumber == 1){
+    _storyNumber = 2;
+  }
+  else if (userChoice == 2 && _storyNumber == 1){
+    _storyNumber = 3;
+  }
+  else if (userChoice == 1 && _storyNumber == 2){
+    _storyNumber = 5;
+  }
+  else if (userChoice == 2 && _storyNumber == 2){
+    _storyNumber = 4;
+  }
+  else if (userChoice == 3 || userChoice == 4 || userChoice == 5){
+    restart();
+  }
+}
+
+String getStory() {
+  return _storyData[_storyNumber].storyTitle;
+}
+
+
+String getChoice1() {
+  return _storyData[_storyNumber].choice1;
+}
+
+String getChoice2() {
+  return _storyData[_storyNumber].choice2;
+}
+
+void restart(){
+  _storyNumber = 0;
+}
+
+
+bool buttonShouldBeVisible(){
+  if (_storyNumber == 0 || _storyNumber == 1 || _storyNumber == 2){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+}
+
+
+
+
+
+//TODO: Step 23 - Use the _storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
+
+//TODO: Step 25 - Change the _storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (_storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
+
+//TODO: Step 21 - Using the story plan, update nextStory() to change the _storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the _storyNumber is 0, the _storyNumber should become 2.
+
+//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if _storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
